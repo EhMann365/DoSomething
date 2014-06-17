3b3f756170825a76d1db2ecf91dde9dcc93a7d01
Brian Widtmann <brian.widtmann@gmail.com>
2014-06-17 23:50:51 +0200

    Start activity created

---
+++ src/com/example/DoSomething/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.DoSomething;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Brian on 17.06.2014.
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.detail);
+    }
+}
