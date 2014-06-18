519ab2e4492ac633b91b4a74fbcd7db731f7a4ab
Coriarius <joshua.tanner.95@gmail.com>
2014-06-18 11:07:03 +0200

    add packages to src folder and add void classes to the packages.

---
+++ src/ch/m335/dao/HomeworkDao.java
@@ -0,0 +1,7 @@
+package ch.m335.dao;
+
+/**
+ * Created by Admin on 18.06.2014.
+ */
+public class HomeworkDao {
+}
---
+++ src/ch/m335/entities/HomeworkItem.java
@@ -0,0 +1,7 @@
+package ch.m335.entities;
+
+/**
+ * Created by Joshua on 18.06.2014.
+ */
+public class HomeworkItem {
+}
