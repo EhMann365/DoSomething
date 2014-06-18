442c34ae56a3ff220461e31b4eb87510d5a59c43
Coriarius <joshua.tanner.95@gmail.com>
2014-06-18 14:37:26 +0200

    Rework of the DetailActivity

--- src/ch/m335/controllers/DetailActivity.java
+++ src/ch/m335/controllers/DetailActivity.java
@@ -2,13 +2,46 @@ package ch.m335.controllers;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
+import ch.m335.controllers.R;
 
 /**
- * Created by Admin on 18.06.2014.
+ * Created by Joshua on 18.06.2014.
  */
 public class DetailActivity extends Activity {
+    private EditText etTitle;
+    private EditText etSubject;
+    private DatePicker etDueDate;
+    private EditText etPicture;
+    private Button btnPicture;
+    private EditText etComment;
+    private View.OnClickListener oclPicture;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.detail);
+
+        init();
+    }
+
+    private void init() {
+        etTitle = (EditText) findViewById(R.id.etTitle);
+        etSubject = (EditText) findViewById(R.id.etSubject);
+        etDueDate = (DatePicker) findViewById(R.id.dpDueDate);
+        etPicture = (EditText) findViewById(R.id.etPicture);
+        btnPicture = (Button) findViewById(R.id.btnPicture);
+        etComment = (EditText) findViewById(R.id.etComment);
+
+        oclPicture = new View.OnClickListener() {
+            public void onClick(View v) {
+
+            }
+        };
+        btnPicture.setOnClickListener(oclPicture);
     }
 }
--- src/ch/m335/controllers/MainActivity.java
+++ src/ch/m335/controllers/MainActivity.java
@@ -1,8 +1,9 @@
 package ch.m335.controllers;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import com.example.DoSomething.R;
+import ch.m335.controllers.R;
 
 /**
  * Created by Brian on 17.06.2014.
@@ -13,5 +14,8 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.detail);
+
+        Intent intent = new Intent(this, DetailActivity.class);
+        startActivity(intent);
     }
 }
